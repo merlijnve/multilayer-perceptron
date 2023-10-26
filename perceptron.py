ed432d4001c3e1e1eba01d08c1ed3e1886e97335
Merlijn van Engelen <merlijnvanengelen@gmail.com>
2023-10-26 16:21:18 +0200

    Add bias to perceptron

--- perceptron.py
+++ perceptron.py
@@ -1,21 +1,22 @@
 import numpy as np
+from numpy import ndarray
 
 
 class Perceptron:
-
     def __init__(self, size):
         self.weights = np.random.uniform(low=-1.0, high=1.0, size=size)
+        self.bias = 1.0
 
-    def estimate(self, inputs: np.ndarray):
+    def estimate(self, inputs: ndarray):
         if len(inputs) != len(self.weights):
             raise ValueError("Number of inputs must match number of weights")
 
-        sum = np.dot(inputs, self.weights)
+        sum = np.dot(inputs, self.weights) + self.bias
         # step/activation function
         output = np.sign(sum)
         return output
 
-    def train(self, inputs: np.ndarray, target: np.ndarray):
+    def train(self, inputs: ndarray, target: ndarray):
         if len(inputs) != len(self.weights):
             raise ValueError("Number of inputs must match number of weights")
 
@@ -23,3 +24,4 @@ class Perceptron:
         error = (target - est)
 
         self.weights += error * inputs
+        self.bias += error
