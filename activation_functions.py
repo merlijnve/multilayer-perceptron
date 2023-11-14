cde334204480ec38405927335b094b4fad781e45
mvan-eng <merlijnvanengelen@gmail.com>
2023-11-14 22:24:36 +0100

    Add derivative of activations

--- activation_functions.py
+++ activation_functions.py
@@ -5,5 +5,13 @@ def relu(x):
     return np.maximum(0.0, x)
 
 
+def relu_derivative(x):
+    return np.heaviside(x, 1.0)
+
+
 def softmax(x):
     return np.exp(x) / np.sum(np.exp(x))
+
+
+def softmax_derivative(x):
+    return softmax(x) * (1 - softmax(x))
