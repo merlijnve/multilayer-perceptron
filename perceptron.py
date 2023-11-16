b4593de266a217ab10cec6fa3092f90f60291c24
mvan-eng <merlijnvanengelen@gmail.com>
2023-11-16 22:10:45 +0100

    Add loss_functions

--- perceptron.py
+++ perceptron.py
@@ -4,7 +4,8 @@ from numpy import ndarray
 
 class Perceptron:
     def __init__(self, size):
-        self.weights = np.random.uniform(low=-1.0, high=1.0, size=size)
+        self.weights = np.random.default_rng(42).uniform(
+            low=-1.0, high=1.0, size=size)
         self.bias = 1.0
         self.learning_rate = 0.1
 
@@ -12,9 +13,9 @@ class Perceptron:
         if len(inputs) != len(self.weights):
             raise ValueError("Number of inputs must match number of weights")
 
-        sum = np.dot(inputs, self.weights) + self.bias
+        weighted_sum = np.dot(inputs, self.weights) + self.bias
         # step/activation function
-        output = np.sign(sum)
+        output = np.sign(weighted_sum)
         return output
 
     def train(self, inputs: ndarray, target: float):
